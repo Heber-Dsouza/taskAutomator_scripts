394286b8cfec598789605ec9a30d8df986b46a1a
HeberDsouza <lourenco.h.colares@gmail.com>
2026-05-26 22:54:12 -0300

    feat: class gnome and application

---
+++ classes/programs/application.py
@@ -0,0 +1,35 @@
+import uuid
+from classes.programs.gnome import Gnome
+from classes.shortcut import Shortcut
+
+class Application(Gnome):
+    running_stack: list[str] = []
+
+    def __init__(self, launch_command:str):
+        super().__init__(launch_command)
+        self.id = str(uuid.uuid4())
+
+    def run(self):
+
+        if self.is_running():
+            self.open()
+        else:
+            self._launch(self.global_id)
+            self.running_stack.insert(0, self.id)
+
+    def is_active_window(self) -> bool:
+        return self._is_active_window_global() and self.id in self.running_stack and self.running_stack[0] == self.id
+
+    def open(self):
+        if not self.is_active_window():
+            self.__relocate_to_active()
+
+    def is_running(self) -> bool:
+        return self.id in self.running_stack
+
+    def __relocate_to_active(self) -> None:
+        if not self._is_running_global():
+            self._active_global()
+        current_item_index = self.running_stack.index(self.id)
+        Shortcut.lx_cycle_app_windows(current_item_index, 2)
+        self.running_stack.insert(0, self.running_stack.pop(current_item_index))
---
+++ classes/programs/gnome.py
@@ -0,0 +1,37 @@
+import uuid
+from classes.shortcut import Shortcut
+from classes.programs.terminal import Terminal
+from models.detailed_delays import DetailedDelays
+
+class Gnome:
+    global_running_stack: list[str] = []
+
+    def __init__(self, launch_command:str):
+        self.global_id:str = str(uuid.uuid4())
+        self.launch_command:str = launch_command
+
+    def _launch(self, global_id):
+
+        if self._is_running_global():
+            self._active_global()
+        else:
+            Terminal.quick_launch(
+                self.launch_command,
+                detailed_delays=DetailedDelays(delay_terminal=1, delay_typing=1, delay_confirm=0))
+            self.global_running_stack.insert(0, global_id)
+
+    def _active_global(self):
+        if not self._is_active_window_global():
+            self.__relocate_to_active_global()
+
+
+    def _is_active_window_global(self) -> bool:
+        return self.global_id in self.global_running_stack and self.global_running_stack[0] == self.global_id
+
+    def _is_running_global(self) -> bool:
+        return self.global_id in self.global_running_stack
+
+    def __relocate_to_active_global(self) -> None:
+        current_index = self.global_running_stack.index(self.global_id)
+        Shortcut.lx_cycle_windows(current_index, 2)
+        self.global_running_stack.insert(0, self.global_running_stack.pop(current_index))
